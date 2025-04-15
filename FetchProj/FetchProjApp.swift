42afcfd98b6df196cc3ed32e18bc10bc77b4005c
Kaleb Davis <kalebdavis03@gmail.com>
2025-04-14 18:08:08 -0700

    Initial Commit

---
+++ FetchProj/FetchProjApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchProjApp.swift
+//  FetchProj
+//
+//  Created by Kaleb Davis on 4/14/25.
+//
+
+import SwiftUI
+
+@main
+struct FetchProjApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
